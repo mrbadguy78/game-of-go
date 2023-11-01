06a6eb1d919178f6905917da25f773b57de1f416
Giuseppe Gibilisco <giuseppe.gibilisco@gmail.com>
2023-11-01 14:40:02 +0000

    Add unit tests for Goban class

--- tests/Unit/GobanTest.php
+++ tests/Unit/GobanTest.php
@@ -43,3 +43,73 @@ describe("getStatus method", function () {
         [Status::BLACK, 2, 2],
     ]);
 });
+
+describe("isTaken method", function () {
+    it('returns false when the goban board array is empty', function () {
+        $goban = new Goban([]);
+
+        $this->assertFalse($goban->isTaken(0, 0));
+    });
+
+    it('returns true because "o" has no freedom, no adjacent empty space', function () {
+        $gobanBoard = [
+            ['.', '#', '.'],
+            ['#', 'o', '#'],
+            ['.', '#', '.'],
+        ];
+
+        $goban = new Goban($gobanBoard);
+
+        $this->assertTrue($goban->isTaken(1, 1));
+    });
+
+    it('returns false because "o" has a freedom over', function () {
+        $gobanBoard = [
+            ['.', '.', '.'],
+            ['#', 'o', '#'],
+            ['.', '#', '.'],
+        ];
+
+        $goban = new Goban($gobanBoard);
+
+        $this->assertFalse($goban->isTaken(1, 1));
+    });
+
+    it('returns true because "o" has no freedom (the top and the left are out of the goban)', function () {
+        $gobanBoard = [
+            ['o', '#', '.'],
+            ['#', '.', '.'],
+            ['.', '.', '.'],
+        ];
+
+        $goban = new Goban($gobanBoard);
+
+        $this->assertTrue($goban->isTaken(0, 0));
+    });
+
+    it('returns true because the form # has no freedom)', function () {
+        $gobanBoard = [
+            ['o', 'o', '.'],
+            ['#', '#', 'o'],
+            ['o', 'o', '#'],
+            ['.', 'o', '.']
+        ];
+
+        $goban = new Goban($gobanBoard);
+
+        $this->assertTrue($goban->isTaken(0, 1));
+    });
+
+    it('returns false because the form # has a freedom in x = 2, y = 1)', function () {
+        $gobanBoard = [
+            ['o', 'o', '.'],
+            ['#', '#', '.'],
+            ['o', 'o', '#'],
+            ['.', 'o', '.']
+        ];
+
+        $goban = new Goban($gobanBoard);
+
+        $this->assertFalse($goban->isTaken(1, 1));
+    });
+});
