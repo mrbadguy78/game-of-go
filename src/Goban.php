5261bfe43aa63118075d1781712f90e0f8b76a7d
Giuseppe Gibilisco <giuseppe.gibilisco@gmail.com>
2023-11-01 14:39:21 +0000

    Add Goban class

--- src/Goban.php
+++ src/Goban.php
@@ -7,22 +7,29 @@ use MrBadGuy\GameOfGo\Status;
 class Goban
 {
     /**
+     * Positions already visited
+     *
+     * @var array
+     */
+    private array $visited;
+
+    /**
      * Class constructor
      *
      * @param array $goban 
      */
     public function __construct(
-        private readonly array $gobanBoard,
+        private readonly array $gobanBoard
     ) {
     }
 
     /**
      * Get the status of a given position
      * 
-     * @param integer $x The x coordinate
-     * @param integer $y The y coordinate
+     * @param integer $x $x The x-coordinate of the stone.
+     * @param integer $y The y-coordinate of the stone.
      * 
-     * @return Status
+     * @return Status The status at the given position
      */
     public function getStatus(int $x, int $y): Status
     {
@@ -40,6 +47,62 @@ class Goban
             '.' => Status::EMPTY,
             'o' => Status::WHITE,
             '#' => Status::BLACK,
+            default => Status::OUT,
         };
+    }
+
+    /**
+     * Determines whether a stone at a given position is taken.
+     *
+     * @param int $x The x-coordinate of the stone.
+     * @param int $y The y-coordinate of the stone.
+     * 
+     * @return bool True if the stone is taken, false otherwise.
+     */
+    public function isTaken(int $x, int $y): bool
+    {
+        $status = $this->getStatus($x, $y);
+
+        if ($status !== Status::BLACK && $status !== Status::WHITE) {
+            return false;
+        }
+
+        return !$this->hasFreedom($x, $y, $status);
+    }
+
+    /**
+     * Determines whether a stone has freedom.
+     * 
+     * @param int $x The x-coordinate of the stone.
+     * @param int $y The y-coordinate of the stone.
+     * @param Status $targetStatus The status at the given position.
+     * 
+     * @return bool True if the stone has freedom, false otherwise.
+     */
+    private function hasFreedom(int $x, int $y, Status $targetStatus): bool
+    {
+        $key = $x . "," . $y;
+
+        if (isset($this->visited[$key])) {
+            return false;
+        }
+
+        $this->visited[$key] = true;
+
+        $status = $this->getStatus($x, $y);
+
+        if ($status === Status::EMPTY) {
+            return true;
+        }
+
+        if ($status !== $targetStatus) {
+            return false;
+        }
+
+        // Check all adjacent positions
+        return $this->hasFreedom($x - 1, $y, $targetStatus) ||
+            $this->hasFreedom($x + 1, $y, $targetStatus) ||
+            $this->hasFreedom($x, $y - 1, $targetStatus) ||
+            $this->hasFreedom($x, $y + 1, $targetStatus);
     }
 }
